6c1eb57d615e42caca07a393aef9eb0fe0062360
Andrew Gingrich <ajgingrich@yahoo.com>
2018-08-20 18:31:44 -0600

    finish fractal tree and linux shell challenges

--- linux-shell/general-bash/fractal-tree.sh
+++ linux-shell/general-bash/fractal-tree.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
 
-read n
+n=5
 baseline=""
 res=()
 height=16
 graphwidth=100
 graphheight=63
 center=$(($graphwidth/2))
+numberbranches=1
+verticaladjustment=0
 
-# columns init
-for x in `seq 1 $graphwidth`; do
-    baseline+="_"
-done
-
-# rows init
-for y in `seq 1 $graphheight`; do
-    res[$y]=$baseline
-done
+# columns and rows init
+for x in `seq 1 $graphwidth`; do baseline+="_"; done
+for y in `seq 1 $graphheight`; do res[$y]=$baseline; done
 
 # modify lines
 for i in `seq 1 $n`; do
-    for c in `seq 1 $(($height))`; do
-        verticaladjustment=$(($height*4*($i-1)))
+    for c in `seq 1 $height`; do
         baseposition=$(($graphheight-$c+1-$verticaladjustment))
         branchline=$(($graphheight-$(($height))-$c+1-$verticaladjustment))
 
-        # create base and branches
-        for b in `seq 1 $i`; do
-          res[$baseposition]=$(echo ${res[$baseposition]} | sed s/./1/$(($center*$b)))
-          res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$(($center*$b-$c)))
-          res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$(($center*$b+$c)))
-        done
+        # create left and right outer branches
+        horizontaladjustment=$(($verticaladjustment/2))
+        res[$baseposition]=$(echo ${res[$baseposition]} | sed s/./1/$(($center-$horizontaladjustment)))
+        res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$((($center-$horizontaladjustment)-$c)))
+        res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$((($center-$horizontaladjustment)+$c)))
+
+        res[$baseposition]=$(echo ${res[$baseposition]} | sed s/./1/$(($center+$horizontaladjustment)))
+        res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$((($center+$horizontaladjustment)-$c)))
+        res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$((($center+$horizontaladjustment)+$c)))
+
+        # inner branches
+        if [ $numberbranches -gt 2 ]; then
+          branchesToMake=$((numberbranches - 2))
+          right=$(($center+$horizontaladjustment-$height*4))
+          left=$(($center-$horizontaladjustment+$height*4))
+
+          for r in `seq 1 $branchesToMake`; do
+            res[$baseposition]=$(echo ${res[$baseposition]} | sed s/./1/$right)
+            res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$(($right-$c)))
+            res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$(($right+$c)))
+
+            res[$baseposition]=$(echo ${res[$baseposition]} | sed s/./1/$left)
+            res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$(($left-$c)))
+            res[$branchline]=$(echo ${res[$branchline]} | sed s/./1/$(($left+$c)))
+
+            right=$(($center+$horizontaladjustment-$height*4*$r))
+            # left=$(($center-$horizontaladjustment+$height*8))
+          done
+        fi
     done
-    center=$(($center-$height))
+    verticaladjustment=$(($verticaladjustment+$height*2))
     height=$(($height/2))
+    numberbranches=$((numberbranches*2))
 done
 
 # print
-for line in ${res[@]}; do
-    echo $line
-done
+for line in ${res[@]}; do echo $line; done
