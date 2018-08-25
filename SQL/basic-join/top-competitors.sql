1f1486c0ab3eabe39d0654f01665cfd70743d88a
Andrew Gingrich <ajgingrich@yahoo.com>
2018-08-25 10:33:44 -0600

    complete top competitors

--- SQL/basic-join/top-competitors.sql
+++ SQL/basic-join/top-competitors.sql
@@ -1,8 +1,7 @@
 select H.hacker_id, H.name from Hackers H
-    inner join Challenges C on H.hacker_id=C.hacker_id
-    inner join Submissions S on S.challenge_id=C.challenge_id
+    inner join Submissions S on S.hacker_id=H.hacker_id
+    inner join Challenges C on C.challenge_id=S.challenge_id
     inner join Difficulty D on C.difficulty_level=D.difficulty_level and D.score=S.score
     group by H.hacker_id, H.name
     having count(H.hacker_id) > 1
-
--- wip
+    order by count(H.hacker_id) DESC, H.hacker_id ASC
