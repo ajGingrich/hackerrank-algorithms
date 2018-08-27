842583fd4ad056f5d73ab717a5cdc26baa2640b4
Andrew Gingrich <ajgingrich@yahoo.com>
2018-08-27 17:38:23 -0600

    complete triangle select

--- SQL/advanced-select/type-triangle.sql
+++ SQL/advanced-select/type-triangle.sql
@@ -1,1 +1,5 @@
-wip
+select case when a + b <= c OR a + c <= b OR b + c <= a then 'Not A Triangle'
+    when a=b and b=c then 'Equilateral'
+    when a=b or b=c or a=c then 'Isosceles'
+    else 'Scalene' end
+    from TRIANGLES
