4e28591410c4468b72abdc819947d674271b45f4
Andrew Gingrich <ajgingrich@yahoo.com>
2018-09-13 12:28:18 -0600

    finish one advanced join

--- SQL/advanced-join/projects.sql
+++ SQL/advanced-join/projects.sql
@@ -1,4 +1,10 @@
---wip
-select M.start_date, M.end_date from projects M
-    join projects P on P.end_date = date_add(M.end_date, interval 1 day)
-    
+SET sql_mode = ''; -- set to default sql mode
+SELECT Start_Date, End_Date
+FROM
+    (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) a, -- get only start dates that don't also have same end date
+    (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) b -- get only end dates that don't also have same start
+WHERE Start_Date < End_Date -- make sure start date comes first
+GROUP BY Start_Date -- only unique start dates
+ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date -- get min start date
+
+--credit to timcottenio discussions
