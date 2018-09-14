c2c84c0eb3cabef381312defa2c40fed57ccc87b
Andrew Gingrich <ajgingrich@yahoo.com>
2018-09-13 20:10:25 -0600

    wip for symmetric pairs

--- SQL/advanced-join/symmetric-pairs.sql
+++ SQL/advanced-join/symmetric-pairs.sql
@@ -1,1 +1,15 @@
 -- wip
+
+select L.x, L.y from functions L
+where L.x in (select C.y from (select x, y from functions where x=2) C)
+and L.y in (select D.x from (select x, y from functions where y=24) D)
+order by L.x
+
+
+select x, y from functions f1
+    where exists(select * from functions f2 where f2.y=f1.x
+    and f2.x=f1.y and f2.x>f1.x) and (x!=y)
+union
+select x, y from functions f1 where x=y and
+    ((select count(*) from functions where x=f1.x and y=f1.x)>1)
+        order by x;
