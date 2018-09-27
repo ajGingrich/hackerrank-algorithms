c71cbe0a2398a8837e99ea95de5c8db1eaf285a9
Andrew Gingrich <ajgingrich@yahoo.com>
2018-09-27 16:22:40 -0600

    finish split phone number

--- regex/applications/split-phone-numbers.js
+++ regex/applications/split-phone-numbers.js
@@ -1,1 +1,20 @@
-// placeholder
+function processData(input) {
+    const lines = input.split('\n')
+    const n = lines.shift()
+
+    for(let i=0; i<n; i++) {
+        const codes = lines[i].split(/[-\s]/)
+        console.log(`CountryCode=${codes[0]},LocalAreaCode=${codes[1]},Number=${codes[2]}`)
+    }
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
