5d24a052ff1bb690f4e89739af2e50c8654ee36b
Andrew Gingrich <ajgingrich@yahoo.com>
2018-10-20 09:08:46 -0600

    reverse string

---
+++ java/strings/string-reverse.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void isPalindrome(String A) {
+        if(A.equals(new StringBuilder(A).reverse().toString())) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        isPalindrome(A);
+    }
+}
