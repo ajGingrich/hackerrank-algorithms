4adf29890ea671114d6e25d17dea4820ed89d7d5
Andrew Gingrich <ajgingrich@yahoo.com>
2018-10-26 12:16:49 -0600

    finish java regex

---
+++ java/strings/regex.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+
+class MyRegex {
+    // taken from discussions
+    // this is a regex test not Java practice and already validated different IP standards in regex section
+    String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+    public String pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
+}
